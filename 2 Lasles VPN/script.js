f04c88be9e1bdc2f8a7f05d97616114e52e38c4a
Sokrat <lumen437@gmail.com>
2020-11-17 16:41:46 +0400

    completed map section

--- 2 Lasles VPN/script.js
+++ 2 Lasles VPN/script.js
@@ -33,3 +33,15 @@ const checkBtns = () => {
 checkBtns();
 
 //map part
+
+let mapPoints = document.querySelectorAll('.map-point');
+
+setInterval(() => {
+    let transformedPoint = mapPoints[Math.floor(Math.random() * mapPoints.length)];
+
+    transformedPoint.style.transform = "scale(2, 2)";
+    setTimeout(() => {
+        transformedPoint.style.transform = "scale(1, 1)";
+    }, 1000)
+
+}, 2500);
